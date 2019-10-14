hquip/frida-gum

commit c507befabbbc2302873406faca9c240e0082bf84

Make linker seeking future-proof on Android (#360)
Per frida/frida#1026 and frida/frida-gum#354 - I looked into how the
linker is meant to be established in Android going forward.

According to

    https://source.android.com/devices/architecture/modular-system/runtime#bionic-changes

Google intends to maintain both `/system/bin/linker` and
`/system/bin/linker64` as a symlink, pointing to what the real file
will be.

This means if we look for those files and they are symlinks, the linker
should be mapped to memory using the files' realpath. So for future
changes, looking to see what the symlink resolves as should make this
work in all future version (and keep its backwards compatibility).

Tested this on Android 5, 7.2, 8.0.1, 9 and 10.

gum/backend-linux/gumandroid.c
@@ -321,8 +321,10 @@ struct _GumFunctionSignature
 };
 
 static const GumModuleDetails * gum_try_init_linker_details (void);
+static gchar * gum_find_linker_path (void);
 static gboolean gum_try_parse_linker_proc_maps_line (const gchar * line,
-    GumModuleDetails * module, GumMemoryRange * range);
+    const gchar * linker_path, GumModuleDetails * module,
+    GumMemoryRange * range);
 
 static gboolean gum_store_module_handle_if_name_matches (
     const GumSoinfoDetails * details, GumGetModuleHandleContext * ctx);
@@ -573,6 +575,7 @@ gum_try_init_linker_details (void)
   const GumModuleDetails * result = NULL;
   gchar * maps, ** lines;
   gint num_lines, vdso_index, i;
+  gchar * linker_path = NULL;
 
   /*
    * Using /proc/self/maps means there might be false positives, as the
@@ -601,10 +604,12 @@ gum_try_init_linker_details (void)
   if (vdso_index == -1)
     goto no_vdso;
 
+  linker_path = gum_find_linker_path ();
+
   for (i = vdso_index + 1; i != num_lines; i++)
   {
-    if (gum_try_parse_linker_proc_maps_line (lines[i], &gum_dl_module,
-        &gum_dl_range))
+    if (gum_try_parse_linker_proc_maps_line (lines[i], linker_path,
+        &gum_dl_module, &gum_dl_range))
     {
       result = &gum_dl_module;
       goto beach;
@@ -613,8 +618,8 @@ gum_try_init_linker_details (void)
 
   for (i = vdso_index - 1; i >= 0; i--)
   {
-    if (gum_try_parse_linker_proc_maps_line (lines[i], &gum_dl_module,
-        &gum_dl_range))
+    if (gum_try_parse_linker_proc_maps_line (lines[i], linker_path,
+        &gum_dl_module, &gum_dl_range))
     {
       result = &gum_dl_module;
       goto beach;
@@ -626,8 +631,8 @@ gum_try_init_linker_details (void)
 no_vdso:
   for (i = num_lines - 1; i >= 0; i--)
   {
-    if (gum_try_parse_linker_proc_maps_line (lines[i], &gum_dl_module,
-        &gum_dl_range))
+    if (gum_try_parse_linker_proc_maps_line (lines[i], linker_path,
+        &gum_dl_module, &gum_dl_range))
     {
       result = &gum_dl_module;
       goto beach;
@@ -635,14 +640,36 @@ no_vdso:
   }
 
 beach:
+  g_free (linker_path);
+
   g_strfreev (lines);
   g_free (maps);
 
   return result;
 }
 
+static gchar *
+gum_find_linker_path (void)
+{
+  gchar * result = NULL;
+  const gchar * traditional_path;
+
+  traditional_path = (sizeof (gpointer) == 4)
+      ? "/system/bin/linker"
+      : "/system/bin/linker64";
+
+  if (g_file_test (traditional_path, G_FILE_TEST_IS_SYMLINK))
+    result = g_file_read_link (traditional_path, NULL);
+
+  if (result == NULL)
+    result = g_strdup (traditional_path);
+
+  return result;
+}
+
 static gboolean
 gum_try_parse_linker_proc_maps_line (const gchar * line,
+                                     const gchar * linker_path,
                                      GumModuleDetails * module,
                                      GumMemoryRange * range)
 {
@@ -650,7 +677,6 @@ gum_try_parse_linker_proc_maps_line (const gchar * line,
   gchar perms[5] = { 0, };
   gchar path[PATH_MAX];
   gint n;
-  const gchar * new_path, * old_path, * linker_path;
   const guint8 elf_magic[] = { 0x7f, 'E', 'L', 'F' };
 
   n = sscanf (line,
@@ -664,22 +690,7 @@ gum_try_parse_linker_proc_maps_line (const gchar * line,
   if (n != 4)
     return FALSE;
 
-  if (sizeof (gpointer) == 4)
-  {
-    new_path = "/apex/com.android.runtime/bin/linker";
-    old_path = "/system/bin/linker";
-  }
-  else
-  {
-    new_path = "/apex/com.android.runtime/bin/linker64";
-    old_path = "/system/bin/linker64";
-  }
-
-  if (strcmp (path, new_path) == 0)
-    linker_path = new_path;
-  else if (strcmp (path, old_path) == 0)
-    linker_path = old_path;
-  else
+  if (strcmp (path, linker_path) != 0)
     return FALSE;
 
   if (perms[0] != 'r')

commit efa878a88cf6679d401a0c77ebb4aeb6b608a59f

Fix crash on Android systems without a vDSO
Another regression introduced in c507bef.

Fixes frida/frida#1048.

gum/backend-linux/gumandroid.c
@@ -576,9 +576,10 @@ static const GumModuleDetails *
 gum_try_init_linker_details (void)
 {
   const GumModuleDetails * result = NULL;
-  gchar * maps, ** lines;
+  gchar * linker_path, * maps, ** lines;
   gint num_lines, vdso_index, i;
-  gchar * linker_path = NULL;
+
+  linker_path = gum_find_linker_path ();
 
   /*
    * Using /proc/self/maps means there might be false positives, as the
@@ -606,8 +607,6 @@ gum_try_init_linker_details (void)
   }
   if (vdso_index == -1)
     goto no_vdso;
-
-  linker_path = gum_find_linker_path ();
 
   for (i = vdso_index + 1; i != num_lines; i++)
   {