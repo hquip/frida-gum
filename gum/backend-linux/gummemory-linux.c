hquip/frida-gum

commit 419633e6c4883e9bd333541255cafc8d7b1ab163

Fix ARM cache flushing on Android 11's translator
Using the compiler built-in results in an inline syscall, which isn't
reliable in all execution environments, such as Android 11's new
translator. Turns out this may go sideways when flushing a range
belonging to libc.so.

It's better to call cacheflush() in libc.so for maximum compatibility.

gum/backend-linux/gummemory-linux.c
@@ -111,7 +111,9 @@ void
 gum_clear_cache (gpointer address,
                  gsize size)
 {
-#if defined (HAVE_ARM) || defined (HAVE_ARM64) || defined (HAVE_MIPS)
+#if defined (HAVE_ANDROID) && defined (HAVE_ARM)
+  cacheflush (GPOINTER_TO_SIZE (address), size, 0);
+#elif defined (HAVE_ARM) || defined (HAVE_ARM64) || defined (HAVE_MIPS)
   __builtin___clear_cache (address, address + size);
 #endif
 

commit 051363cd6710e23c05491c1f86e5e7a9c3edb4ca

Fix ARM cache flushing on Android
Turns out cacheflush() expects a range on Linux/ARM.

This regression was introduced in 419633e.

gum/backend-linux/gummemory-linux.c
@@ -112,7 +112,7 @@ gum_clear_cache (gpointer address,
                  gsize size)
 {
 #if defined (HAVE_ANDROID) && defined (HAVE_ARM)
-  cacheflush (GPOINTER_TO_SIZE (address), size, 0);
+  cacheflush (GPOINTER_TO_SIZE (address), GPOINTER_TO_SIZE (address + size), 0);
 #elif defined (HAVE_ARM) || defined (HAVE_ARM64) || defined (HAVE_MIPS)
   __builtin___clear_cache (address, address + size);
 #endif